53b18903039a25abe919393101dbdb0b9965baa7
Ibtisam <81363048+iuhmirza@users.noreply.github.com>
2022-09-03 17:17:36 +0100

    Refactor code

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,49 +1,44 @@
 const Header = ({course}) => <h1>{course}</h1>
 
+const Content = ({parts}) => parts.map(part => <Part key={part.id} part={part}/>)
 
-const Content = ({parts}) => (
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Total = ({parts}) => <p>{parts.map((item) => item.exercises).reduce((a, b) => a+b)}</p>
+
+const Course = ({course}) => (
   <div>
-    <Part part = {parts[0]}/>
-    <Part part = {parts[1]}/>
-    <Part part = {parts[2]}/>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
-)
-
-const Part = ({part}) => (
-  <p>
-  {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({parts}) => (
-  <p>{parts.map((item) => item.exercises).reduce((a, b) => a+b)}</p>
 )
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <Course course={course}/>
   )
 }
 
