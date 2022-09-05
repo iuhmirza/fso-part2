1d584629bc759fd62a99ad7607890356f9783359
ibtisam <iuhmirza@gmail.com>
2022-09-05 10:54:02 +0100

    Remove unnecessary files

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -23,3 +23,28 @@ function App() {
 }
 
 export default App;
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+    </div>
+  )
+}
+
+export default App
