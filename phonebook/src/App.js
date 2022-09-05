8e312529b0c159f02782db5f52162e5122731fbb
ibtisam <iuhmirza@gmail.com>
2022-09-05 11:18:08 +0100

    Implement adding names to phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,18 +6,34 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value = {newName}
+            onChange={handlePersonChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
