6365a909e3646821ec67ffdabaa20933d257daa8
ibtisam <iuhmirza@gmail.com>
2022-09-07 11:00:14 +0100

    Refactor backend communication into own module

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = ({filter, handleFilterChange}) => {
   return (
@@ -48,8 +48,8 @@ const App = () => {
   const [filter, setNewFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -65,8 +65,8 @@ const App = () => {
     if(persons.some(person => person.name === newPerson.name)) {
       alert(`${newPerson.name} is already added to the phonebook`)
     } else {
-      axios
-        .post('http://localhost:3001/persons', newPerson)
+      personService
+        .create(newPerson)
         .then(response => setPersons(persons.concat(response.data)))
     }
   }
